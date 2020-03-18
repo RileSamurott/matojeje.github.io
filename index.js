8b45f82a27e2055f7e4dc32a898a8b1afaba7d21
Matojeje <maty251@gmail.com>
2020-03-18 22:54:06 +0100

    Started work on desktop view + small improvements

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-function loaded() {
+document.addEventListener("DOMContentLoaded", function() {
     M.AutoInit();
-}
+    document.querySelectorAll(".hidden-before-loaded").forEach(el => {
+        el.classList.remove("hidden-before-loaded")
+    })
+})
 
 function closeSidenav(n) {
     M.Sidenav.getInstance(document.getElementsByClassName("sidenav")[n]).close();
+}
+
+window.addEventListener("resize", function() {
+    let mobileCarousel = document.querySelector("#mobile .carousel");
+    if (isVisible(mobileCarousel)) {
+        M.Carousel.getInstance(mobileCarousel).destroy();
+        M.Carousel.init(mobileCarousel)
+    }
+})
+
+function isVisible(el) {
+    return !!el.offsetParent;
 }
